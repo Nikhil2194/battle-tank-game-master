dbceb2ca8c87d666fbda4a3e449e9a7bd7f2c2b5
Nikhil Dahake <nikhil.dahake@blackmarchstudios.com>
2022-07-10 12:08:32 +0530

    Initial commit

---
+++ Assets/Scripts/GenericSingletonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericSingletonScript<T> : MonoBehaviour where T : MonoBehaviour
+{
+	public static T Instance { get; private set; }
+
+	private void Awake()
+    {
+		if(Instance == null)
+        {
+            Instance = (T)(FindObjectOfType(typeof(T)));
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+}
